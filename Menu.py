81b76d34c53b1d948dc33ec014b9a7ffb5a12e65
Slawa Ostrowski <vlacheslav.ostrowski@lgs-hu.eu>
2026-03-27 15:53:07 +0100

    noch nicht fertig

--- Menu.py
+++ Menu.py
@@ -1,26 +1,32 @@
 from Benutzer import Benutzer
-
+import json 
 class Menu:
 
     def __init__(self):
-        self.__benutzer = None
+        
+        self.__benutzer = []  #eine Liste erstelle fuer mehrere benutzer
 
-    def neuBenutzer(self):
+        self.laden()
+
+    #registrieren neue benutzer
+    def neuBenutzer(self):  
         print("Registrierung")
         name = input("geben Sie username ein: ")
         pas = input("und password: ")
-
-        if self.__benutzer is not None:
-            if name == self.__benutzer.name: 
+        # schleife statt abfrage, damit allebenutzer ueberpruefen
+        for b in self.__benutzer:
+            #b ist immer ein benutzer
+            if name == b.name:
                 print("benutzer existiert bereits!")
                 return
-        self.__benutzer = Benutzer(name, pas)
+            #.append fügt benutzer hinzu
+        self.__benutzer.append(Benutzer(name, pas))
         print("Benutzer anlegen!")
 
 
     def anmelden(self):
-
-        if self.__benutzer is None:
+        print("debug:", self.__benutzer)
+        if not self.__benutzer:
             print('kein benutzer, bitte registrieren zuerst')
             return
         
@@ -29,20 +35,35 @@ class Menu:
         for i in range(3):
             username = input("username: ")
             password = input("password: ")
-
-            if username == self.__benutzer.name and password == self.__benutzer.pas:
-                print("anmeldung erfolgreich")
-                return
-            else:
-                print("sie haben", i+1, "von 3 versuch gemacht")
+            #füge da auch schleife hinzu
+            for b in self.__benutzer:
+                if username == b.name and password == b.pas:
+                    print("anmeldung erfolgreich")
+                    return
+            
+            print("sie haben", i+1, "von 3 versuch gemacht")
         
         print("sie haben nur 3 versuche")
+
+    def speichern(self):
+        daten = [{"name": b.name, "pas": b.pas} for b in self.__benutzer]
+        with open("users.json", "w") as f:
+            json.dump(daten, f)
+
+    def laden(self):
+        try: 
+            with open("users.json", "r") as f:
+                daten = json.load(f)
+                self.__benutzer = [Benutzer(d["name"], d["pas"]) for d in daten]
+        except: 
+            self.__benutzer = []
 
 
     def beenden(self):
     
         print("beenden")
 
+        self.speichern()
 
     def Hauptmenu(self):
         ende = False
@@ -64,7 +85,6 @@ class Menu:
                 ende = True
             else: 
                 print("keine ahnung was ist das ")
-
 
 
 # start
